e173805485c76e1fd682ff44de65e35dd8c68395
comaychiendau <a1902421@adelaide.edu.au>
2025-05-05 03:45:45 +0930

    fix sr.h according to the gradescope log

--- sr.h
+++ sr.h
@@ -1,51 +1,23 @@
 #ifndef SR_H
 #define SR_H
 
-#include <stdbool.h>
-
+/* Constants for Selective Repeat */
 #define RTT  16.0
 #define WINDOWSIZE 6
-#define SEQSPACE 12   // must be at least 2 * WINDOWSIZE for Selective Repeat
+#define SEQSPACE 12   /* Must be at least 2 * WINDOWSIZE */
 #define NOTINUSE (-1)
 
 #define A 0
 #define B 1
 
-// Message passed from layer 5 to layer 4
-struct msg {
-    char data[20];
-};
-
-// Packet passed between layer 4 and layer 3
-struct pkt {
-    int seqnum;
-    int acknum;
-    int checksum;
-    char payload[20];
-};
-
-// Function declarations for sender and receiver logic
+/* Function declarations (defined in sr.c) */
 void A_init(void);
-void B_init(void);
-void A_output(struct msg message);
-void A_input(struct pkt packet);
+void A_output(struct msg);
+void A_input(struct pkt);
 void A_timerinterrupt(void);
-void B_input(struct pkt packet);
-void B_output(struct msg message);
-void B_timerinterrupt(void);
+void B_init(void);
+void B_input(struct pkt);
+void B_output(struct msg);     /* Optional */
+void B_timerinterrupt(void);   /* Optional */
 
-// Provided by emulator
-extern void tolayer3(int AorB, struct pkt packet);
-extern void tolayer5(int AorB, char data[20]);
-extern void starttimer(int AorB, double increment);
-extern void stoptimer(int AorB);
-
-// Trace and statistics
-extern int TRACE;
-extern int total_ACKs_received;
-extern int packets_resent;
-extern int new_ACKs;
-extern int packets_received;
-extern int window_full;
-
-#endif // SR_H
+#endif /* SR_H */
