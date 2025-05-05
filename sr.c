8545bce7b44e5b0ef6dc0920f77a3c0fe63111f9
comaychiendau <a1902421@adelaide.edu.au>
2025-05-05 12:17:16 +0930

    commit 11 Applying SRlogic for Receiver B: packet buffering, ACKs, and in-order delivery
    - Add B_buffer[] to store received packets by sequence number
    - Add received[] flags to track received sequence numbers
    - B_input now buffers out-of-order packets and delivers in-order ones when possible
    - Sends ACK for every correct packet (even duplicates)
    - B_base updated only when contiguous packets are delivered
    - Fixed B_init() to properly initialize B_base and received array

--- sr.c
+++ sr.c
@@ -174,64 +174,46 @@ void A_init(void)
 
 
 /********* Receiver (B)  variables and procedures ************/
-
-static int expectedseqnum; /* the sequence number expected next by the receiver */
-static int B_nextseqnum;   /* the sequence number for the next packets sent by B */
-static struct pkt receiver_buffer[WINDOWSIZE];
-static int B_windowfirst = 0;
+static int B_base=0; /*base*/
+static struct pkt B_buffer[SEQSPACE]; /*store received packets*/
+static bool received[SEQSPACE]; /*flag to track which seq has been recieved*/
 
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
   struct pkt sendpkt;
-  int i;
 
   /* if not corrupted and received packet is in order */
-  if  ( (!IsCorrupted(packet))  && (packet.seqnum == expectedseqnum) ) {
-    if (TRACE > 0)
-      printf("----B: packet %d is correctly received, send ACK!\n",packet.seqnum);
-    packets_received++;
-
-    /* deliver to receiving application */
-    tolayer5(B, packet.payload);
-
-    /* send an ACK for the received packet */
-    sendpkt.acknum = expectedseqnum;
-
-    /* update state variables */
-    expectedseqnum = (expectedseqnum + 1) % SEQSPACE;
-  }
-  else {
-    /* packet is corrupted or out of order resend last ACK */
-    if (TRACE > 0)
-      printf("----B: packet corrupted or not expected sequence number, resend ACK!\n");
-    if (expectedseqnum == 0)
-      sendpkt.acknum = SEQSPACE - 1;
-    else
-      sendpkt.acknum = expectedseqnum - 1;
-  }
-
-  /* create packet */
-  sendpkt.seqnum = B_nextseqnum;
-  B_nextseqnum = (B_nextseqnum + 1) % 2;
-
-  /* we don't have any data to send.  fill payload with 0's */
-  for ( i=0; i<20 ; i++ )
-    sendpkt.payload[i] = '0';
-
-  /* computer checksum */
-  sendpkt.checksum = ComputeChecksum(sendpkt);
-
-  /* send out packet */
-  tolayer3 (B, sendpkt);
+  if (!IsCorrupted(packet)){
+    int sequence = packet.seqnum; /*declare varibale to manage and call easier*/
+    /*if not processed and the seqnum stays within the window*/
+    if (!received[sequence] && ((sequence - B_base + SEQSPACE)%SEQSPACE <WINDOWSIZE)){
+       /*Save and marked as received*/
+        B_buffer[sequence] = packet; 
+        received[sequence] = true;
+        packets_received++;
+        
+        /*Once received send to layer 5*/
+        while (received[B_base]){
+            tolayer5(B, B_buffer[B_base].payload);
+            received[B_base]=false;
+            B_base = (B_base + 1) % SEQSPACE; /*move to the next seq num*/
+        }
+    }
+    /*ACK packet*/
+    sendpkt.seqnum = 0;
+    sendpkt.acknum = packet.seqnum;
+    tolayer3(B, sendpkt);
+    }
 }
-
 /* the following routine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init(void)
 {
-  expectedseqnum = 0;
-  B_nextseqnum = 1;
+    for (int i = 0; i < SEQSPACE; i++){
+        received[i] = false;
+    B_base = 0;
+    }
 }
 
 /******************************************************************************
